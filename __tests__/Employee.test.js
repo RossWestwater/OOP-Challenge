06087cf23d68877069abaa52bcbc526aaee45369
Ross Westwater <ross.westwater@gmail.com>
2022-01-02 10:01:45 -0700

    initial commit w a few updated tests for employee and manager passing

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,15 @@
+const Employee = require('./Employee');
+
+test('properties of employee object', () => {
+  const employee = new Employee('George', '0893', 'george@station.com');
+  expect(employee.name).toEqual('George');
+  expect(employee.id).toEqual('0893');
+  expect(employee.email).toEqual('george@station.com');
+})
+test('methods of employee object', () => {
+  const employee = new Employee('Stacy', '1125', 'stacy@badass.com');
+  expect(employee.getName()).toEqual('Stacy');
+  expect(employee.getId()).toEqual('1125');
+  expect(employee.getEmail()).toEqual('stacy@badass.com');
+  expect(employee.getRole()).toEqual('Employee');
+})
